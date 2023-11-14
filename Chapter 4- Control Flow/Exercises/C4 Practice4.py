2fb71a10ba46c039a83efd26f583212e75619493
sasha <144213798+sxsha777@users.noreply.github.com>
2023-11-14 09:19:36 +0400

    Chapter 4

--- Chapter 4- Control Flow/Exercises/C4 Practice4.py
+++ Chapter 4- Control Flow/Exercises/C4 Practice4.py
@@ -3,7 +3,7 @@ variable is within the range of 24 to 56. If the speed variable's value is outsi
 display 'Speed is abnormal'''
 
 speed=int(input("Enter speed: "))
-if speed > 24 or speed < 56:
+if speed > 24 and speed < 56:
     print("speed is within normal range.")
 else:
     print("speed is outside normal range.")
